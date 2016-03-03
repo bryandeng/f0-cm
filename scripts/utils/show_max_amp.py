ef1eecaa7fe3003f6c784ce32a0061eb037fe613
Boyuan Deng <bryanhsudeng@gmail.com>
2016-03-03 10:41:11 +0100

    Add script showing maximum amplitude over dataset

---
+++ scripts/utils/show_max_amp.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+from multiprocessing import Pool
+
+dataset = "/home/bdeng/datasets/speechdata_16kHz"
+
+wav_paths = []
+
+for root, dirs, files in os.walk(dataset):
+    for name in files:
+        if name.startswith('mic'):
+            wav_paths.append(os.path.join(root, name))
+
+
+def vol_adjustment(src):
+    vol_adjustment = subprocess.run(
+        ["sox", src, "-n", "stat", "-v"],
+        stderr=subprocess.PIPE,
+        universal_newlines=True
+    ).stderr
+    return float(vol_adjustment[:-1])
+
+pool = Pool()
+vol_adjustments = pool.map(vol_adjustment, wav_paths)
+max_amp = 1 / min(vol_adjustments)
+
+print('Minimum volume adjustment:', min(vol_adjustments))
+print('Maximum volume adjustment:', max(vol_adjustments))
+print('Maximum amplitude over dataset:', max_amp)
