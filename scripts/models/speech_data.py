adfffe62f34e8bc090c4b36d936c643bfc00f065
Boyuan Deng <contact@boyuandeng.me>
2016-04-12 17:32:37 +0200

    Add data loading utility

---
+++ scripts/models/speech_data.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import pandas as pd
+from sklearn import cross_validation
+
+hdf5_path = "/home/bdeng/datasets/f0_estimation_data.h5"
+df = pd.read_hdf(hdf5_path, 'f0_estimation')
+
+mfcc_values = df.xs('mfcc', axis=1, level=0).values
+
+
+def load_data(method, test_split=0.1):
+    estimated_values = df.loc[:, ('estimated', method)].values
+    X = np.concatenate((mfcc_values, np.reshape(estimated_values, (-1, 1))),
+                       axis=1)
+    y = df.loc[:, ('correctness', method)].values.astype(int)
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+        X, y, test_size=test_split, random_state=0)
+    return (X_train, y_train), (X_test, y_test)
