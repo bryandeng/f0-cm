9939511ba2c323dbbf5f45fb81ebc0b0b32b845f
Boyuan Deng <contact@boyuandeng.me>
2016-06-13 17:30:03 +0200

    Update MLP model

--- scripts/models/mlp.py
+++ scripts/models/mlp.py
@@ -3,11 +3,13 @@
 import argparse
 import os
 
+from keras.callbacks import EarlyStopping
 from keras.layers import Dense, Dropout
 from keras.models import Sequential
 from keras.optimizers import SGD
 from keras.utils.visualize_util import plot
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import (confusion_matrix, normalized_mutual_info_score,
+                             roc_auc_score)
 
 from speech_data import load_shuffled_points
 
@@ -24,7 +26,7 @@ method = args.method
 weights_file = os.path.join('shelf', 'mlp_model_weights-' + method + '.h5')
 
 model = Sequential()
-model.add(Dense(128, input_dim=14, init='uniform', activation='relu'))
+model.add(Dense(128, input_dim=59, init='uniform', activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
@@ -40,17 +42,22 @@ plot(model, to_file='shelf/model-mlp.png', show_shapes=True)
 if os.path.exists(weights_file):
     model.load_weights(weights_file)
 else:
+    earlystopper = EarlyStopping()
     model.fit(X_train, y_train,
               nb_epoch=40,
               batch_size=batch_size,
-              validation_split=0.2)
+              validation_split=0.2,
+              callbacks=[earlystopper])
     model.save_weights(weights_file)
 
-predicted_classes = model.predict_classes(X_test, batch_size=batch_size)
+print('Algorithm:', method)
 loss_and_metrics = model.evaluate(X_test, y_test, batch_size=batch_size)
-
-print('Test loss:', loss_and_metrics[0])
 print('Test accuracy:', loss_and_metrics[1])
 
+predicted_classes = model.predict_classes(X_test, batch_size=batch_size)
+predicted_probas = model.predict_proba(X_test, batch_size=batch_size)
+print('ROC AUC score:', roc_auc_score(y_test, predicted_probas))
+print('Normalized Mutual Information (NMI):',
+      normalized_mutual_info_score(y_test, predicted_classes.flatten()))
 print('Confusion matrix:')
 print(confusion_matrix(y_test, predicted_classes))
