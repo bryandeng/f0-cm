e491ce13a20124e9fc8868b65913bf582003d23d
Boyuan Deng <contact@boyuandeng.me>
2016-07-28 17:38:08 +0200

    Update plots for classification metrics

--- scripts/models/extra_testing_plot.py
+++ scripts/models/extra_testing_plot.py
@@ -8,19 +8,50 @@ import matplotlib.pyplot as plt
 
 methods = ['martin', 'swipe', 'yin']
 snrs = [20, 15, 10, 5, 0]
-metric_names = ['accuracy', 'roc_auc', 'nmi']
-metric_indices = {'accuracy': 0, 'roc_auc': 1, 'nmi': 2}
+metric_names = ['classification_accuracy', 'roc_auc', 'nmi']
+metric_indices = {'classification_accuracy': 0, 'roc_auc': 1, 'nmi': 2}
 
 with shelve.open(os.path.join('shelf', 'metrics.shelve')) as db:
+    mlp_metrics = db['mlp_metrics']
+    lstm_metrics = db['lstm_metrics']
     mlp_metrics_on_voiced = db['mlp_metrics_on_voiced']
     mlp_metrics_on_unvoiced = db['mlp_metrics_on_unvoiced']
     lstm_metrics_on_voiced = db['lstm_metrics_on_voiced']
     lstm_metrics_on_unvoiced = db['lstm_metrics_on_unvoiced']
 
-locs = {'accuracy': 'lower right', 'roc_auc': 'lower right',
+locs = {'classification_accuracy': 'lower right', 'roc_auc': 'lower right',
         'nmi': 'upper left'}
 
 for method, metric_name in product(methods, metric_names):
+    fig = plt.figure()
+    fig.suptitle(
+        "Classification metrics on distorted audios \n"
+        "(" + method + "'s data, " + metric_name + ")",
+        fontsize=14, fontweight='bold')
+    ax = fig.add_subplot(111)
+    ax.set_xlabel("signal-to-noise ratio")
+    ax.set_ylabel(metric_name)
+    ax.set_ylim([0, 1])
+
+    ax.plot(
+        snrs,
+        [mlp_metrics[(method, snr)][metric_indices[metric_name]]
+         for snr in snrs],
+        color='red', linestyle='solid', marker='o', markersize=4,
+        label="MLP")
+    ax.plot(
+        snrs,
+        [lstm_metrics[(method, snr)][metric_indices[metric_name]]
+         for snr in snrs],
+        color='blue', linestyle='solid', marker='o', markersize=4,
+        label="LSTM")
+
+    ax.legend(loc=locs[metric_name])
+    plt.savefig(os.path.join('../../gallery',
+                             'classification_metrics_whole_' +
+                             method + '_' + metric_name + '.pdf'),
+                papertype='a4')
+
     fig = plt.figure()
     fig.suptitle(
         "Classification metrics on distorted audios \n"
@@ -58,8 +89,8 @@ for method, metric_name in product(methods, metric_names):
 
     ax.legend(loc=locs[metric_name])
     plt.savefig(os.path.join('../../gallery',
-                             'classification_metrics' + '_' + method + '_' +
-                             metric_name + '.pdf'),
+                             'classification_metrics_voicedness_' +
+                             method + '_' + metric_name + '.pdf'),
                 papertype='a4')
 
 print("Done.")
